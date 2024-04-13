a150d3d00e6ff2e61df5b770d8fc134594f16864
Hexalyse <reversebob@gmail.com>
2024-04-13 17:48:08 +0200

    format with black

--- utils.py
+++ utils.py
@@ -6,12 +6,15 @@ import cv2
 import numpy as np
 
 last_frame = None
+
+
 def grab_frame(camera):
     global last_frame
     frame = camera.grab()
     if frame is not None:
-      last_frame = frame
+        last_frame = frame
     return last_frame
+
 
 def smooth_move_to(target_x, target_y, duration_ms):
     start_x, start_y = interception.mouse_position()
@@ -36,27 +39,44 @@ def smooth_move_to(target_x, target_y, duration_ms):
     # Ensure the mouse ends up at the exact target position
     interception.move_to(target_x, target_y)
 
+
 def organic_move_to(target_x, target_y, duration_ms, delay_after=0.0):
     start_x, start_y = interception.mouse_position()
     steps = int(duration_ms / 10)
     distance_x = abs(target_x - start_x)
     distance_y = abs(target_y - start_y)
-    
+
     # Control points for Bezier curve, with random deviation
-    mid_x = (start_x + target_x) / 2 + random.randint(int(-distance_x / 7), int(distance_x / 7))
-    mid_y = (start_y + target_y) / 2 + random.randint(int(-distance_y / 7), int(distance_y / 7))
+    mid_x = (start_x + target_x) / 2 + random.randint(
+        int(-distance_x / 7), int(distance_x / 7)
+    )
+    mid_y = (start_y + target_y) / 2 + random.randint(
+        int(-distance_y / 7), int(distance_y / 7)
+    )
 
     # Adding horizontal deviation if the movement is mostly vertical
-    if distance_x <= distance_y / 4:  # if the x distance is significantly less than the y distance
-        mid_x += random.randint(int(-distance_y / 7), int(distance_y / 7))  # horizontal deviation
+    if (
+        distance_x <= distance_y / 4
+    ):  # if the x distance is significantly less than the y distance
+        mid_x += random.randint(
+            int(-distance_y / 7), int(distance_y / 7)
+        )  # horizontal deviation
 
     for i in range(steps):
         progress = i / steps
         progress -= math.sin(2 * math.pi * progress) / (2 * math.pi)
 
         one_minus_t = 1 - progress
-        current_x = (one_minus_t * one_minus_t * start_x + 2 * one_minus_t * progress * mid_x + progress * progress * target_x)
-        current_y = (one_minus_t * one_minus_t * start_y + 2 * one_minus_t * progress * mid_y + progress * progress * target_y)
+        current_x = (
+            one_minus_t * one_minus_t * start_x
+            + 2 * one_minus_t * progress * mid_x
+            + progress * progress * target_x
+        )
+        current_y = (
+            one_minus_t * one_minus_t * start_y
+            + 2 * one_minus_t * progress * mid_y
+            + progress * progress * target_y
+        )
 
         # Move the mouse to the new position
         interception.move_to(int(current_x), int(current_y))
@@ -68,7 +88,9 @@ def organic_move_to(target_x, target_y, duration_ms, delay_after=0.0):
         time.sleep(delay_after)
 
 
-def matchTemplate_with_threshold(patch, image, similarity_threshold, debug=False, showMatched=False):
+def matchTemplate_with_threshold(
+    patch, image, similarity_threshold, debug=False, showMatched=False
+):
     # Ensure the input threshold is within the expected range
     if not (0 <= similarity_threshold <= 1):
         raise ValueError("The similarity threshold must be between 0 and 1.")
@@ -90,7 +112,10 @@ def matchTemplate_with_threshold(patch, image, similarity_threshold, debug=False
     # Return True if the best match meets or exceeds the percentage threshold, otherwise False
     return max_val >= similarity_threshold
 
-def findAllTemplate_with_threshold(patch, image, similarity_threshold, debug=False, showMatched=False):
+
+def findAllTemplate_with_threshold(
+    patch, image, similarity_threshold, debug=False, showMatched=False
+):
     # Ensure the input threshold is within the expected range
     if not (0 <= similarity_threshold <= 1):
         raise ValueError("The similarity threshold must be between 0 and 1.")
@@ -113,6 +138,7 @@ def findAllTemplate_with_threshold(patch, image, similarity_threshold, debug=Fal
 
     # Return True if the best match meets or exceeds the percentage threshold, otherwise False
     return locations
+
 
 def get_center_of_match(location, patch):
     return location[0] + patch.shape[0] / 2, location[1] + patch.shape[1] / 2
